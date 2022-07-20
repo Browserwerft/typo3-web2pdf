c6f98018fbd8c40b3729b30f6d762e4332e991c0
marcelBaklouti <marcel@baklouti.de>
2022-07-20 14:17:50 +0200

    downgrade mpdf

--- Classes/View/PdfView.php
+++ Classes/View/PdfView.php
@@ -137,8 +137,8 @@ class PdfView
         ]);
 
         // remove this on production
-        $pdf->showImageErrors = true;
-        $pdf->debug = true;
+        #$pdf->showImageErrors = true;
+        #$pdf->debug = true;
 
         $pdf->SetMargins($leftMargin, $rightMargin, $topMargin);
 
